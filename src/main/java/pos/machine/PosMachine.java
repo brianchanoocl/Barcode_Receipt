694c60c81b47d8fa21271fdb43430f76db32437f
brianchanoocl <Delubdi118>
2021-11-30 19:36:29 +0800

    [Barcode_Receipt] [Brian] -feat init repository

---
+++ src/main/java/pos/machine/PosMachine.java
@@ -0,0 +1,95 @@
+package pos.machine;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class ReceiptItem {
+    private final String name;
+    private final int quantity;
+    private final int price;
+
+
+    public ReceiptItem(String name, int quantity, int price) {
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
+
+public class PosMachine {
+    public String printReceipt(List<String> barcodes) {
+        System.out.println("function starts");
+
+
+
+        List<String> digestedBarcodes = digestBarcodes(barcodes);
+        int[] quantityList = digestBarcodesQuantity(digestedBarcodes, barcodes);
+
+
+        List<ItemInfo> shoppingCart = new ArrayList<>();
+        shoppingCart = gatherItemInfo(digestedBarcodes);
+        String receipt = generateReceipt(shoppingCart, quantityList);
+        return receipt;
+    }
+
+    private List<String> digestBarcodes(List<String> barcodes) {
+        List<String> digestedBarcodes = new ArrayList<>();
+        Boolean repeated = false;
+        for(String item : barcodes){
+            if( digestedBarcodes.size() < 1 )
+                digestedBarcodes.add(item);
+            else{
+                repeated = false;
+                for (int i=0 ; i<digestedBarcodes.size() ; i++) {
+                    if( digestedBarcodes.get(i).equals(item) )
+                        repeated = true;
+                }
+                if(!repeated)
+                    digestedBarcodes.add(item);
+            }
+        }
+        return digestedBarcodes;
+    }
+
+
+    private int[] digestBarcodesQuantity(List<String> digestedBarcodes, List<String> barcodes) {
+        return null;
+    }
+
+    private List<ItemInfo> gatherItemInfo(List<String> barcodes) {
+        return null;
+    }
+
+    private String fetchNameByBarcode(String barcode) {
+        return null;
+    }
+
+    private int fetchPriceByBarcode(String barcode) {
+        return 0;
+    }
+
+    private String generateReceipt(List<ItemInfo> shoppingCart, int[] quantityList) {
+        return null;
+    }
+
+    private String generateReceiptLine(ItemInfo shoppingCartItem, int quantity) {
+        return null;
+    }
+
+    private int fetchSubtotal(ItemInfo shoppingCartItem) {
+        return 0;
+    }
+
+}
